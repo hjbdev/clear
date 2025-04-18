bca65466e33ea264b7c5b7f6ea50fc288206d702
Harry Bayliss <harry@hjb.dev>
2025-04-19 00:20:31 +0100

    manifest name

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+    manifest: {
+        name: 'Clear',
+    },
     extensionApi: "chrome",
     modules: ["@wxt-dev/module-vue"],
 });
